a2ce98ac19c583cac5683aa095f3eab0065277b9
kishan-1285 <kishanragunathan26@gmail.com>
2026-08-06 09:21:26 +0530

    forms task 1

--- form_task_1/src/form.jsx
+++ form_task_1/src/form.jsx
@@ -9,47 +9,47 @@ function Form() {
         address: "",
     });
 
-    const handleNamechange = (e) => {
+    const handlechange = (e) => {
         setFormData({
             ...formData,
-            name: e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
-    const handleEmailchange = (e) => {
-        setFormData({
-            ...formData,
-            email: e.target.value
-        });
-    }
+    // const handleEmailchange = (e) => {
+    //     setFormData({
+    //         ...formData,
+    //         email: e.target.value
+    //     });
+    // }
 
-    const handleAgechange = (e) => {
-        setFormData({
-            ...formData,
-            age: e.target.value
-        });
-    }
+    // const handleAgechange = (e) => {
+    //     setFormData({
+    //         ...formData,
+    //         age: e.target.value
+    //     });
+    // }
 
-    const handleGenderchange = (e) =>{
-        setFormData({
-            ...formData,
-            gender:e.target.value
-        })
-    }
+    // const handleGenderchange = (e) =>{
+    //     setFormData({
+    //         ...formData,
+    //         gender:e.target.value
+    //     })
+    // }
 
-    const handleCoursechange = (e)=>{
-        setFormData({
-            ...formData,
-            course:e.target.value
-        })
-    }
+    // const handleCoursechange = (e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         course:e.target.value
+    //     })
+    // }
 
-    const handleAddresschange =(e)=>{
-        setFormData({
-            ...formData,
-            address:e.target.value
-        })
-    }
+    // const handleAddresschange =(e)=>{
+    //     setFormData({
+    //         ...formData,
+    //         address:e.target.value
+    //     })
+    // }
 
     return (
         <>
@@ -60,28 +60,28 @@ function Form() {
             <h1>{formData.course}</h1>
             <h1>{formData.address}</h1>
 
-            <form>
+            <form className="border-2 border-blue-500 p-5 rounded-lg w-96 mt-10">
                 <label>Name</label>
-                <input placeholder="Enter your name" onChange={handleNamechange} value={formData.name} />
+                <input placeholder="Enter your name" onChange={handlechange} name='name' value={formData.name} className="border ml-5"/>
                 <br /><br />
 
                 <label>Email</label>
-                <input placeholder="Enter your email" onChange={handleEmailchange} value={formData.email} />
+                <input placeholder="Enter your email" onChange={handlechange} name='email' value={formData.email} className="border ml-5"/>
                 <br /><br />
 
                 <label>Age</label>
-                <input placeholder="Enter your age" type="number" onChange={handleAgechange} value={formData.age} />
+                <input placeholder="Enter your age" type="number" onChange={handlechange} name='age' value={formData.age} className="border ml-5"/>
                 <br /><br />
 
                 <label>Gender</label>
-                <input type="radio" name="gender" value="Male" onChange={handleGenderchange} />
+                <input type="radio" name="gender" value="Male"  onChange={handlechange} className="border ml-5"/>
                 <label>Male</label>
-                <input type="radio" name="gender" value="Female" onChange={handleGenderchange} />
+                <input type="radio" name="gender" value="Female" onChange={handlechange} className="border ml-5"/>
                 <label>Female</label>
                 <br /><br />
 
                 <label>Course</label>
-                <select onChange={handleCoursechange} >
+                <select onChange={handlechange} name="course"className="border ml-5" >
                     <option value='CSE'>CSE</option>
                     <option value='ECE'>ECE</option>
                     <option value='AIDS'>AIDS</option>
@@ -90,10 +90,10 @@ function Form() {
                 <br /><br />
 
                 <label>Address</label>
-                <textarea rows="4" cols="30" onChange={handleAddresschange}></textarea>
+                <textarea rows="4" cols="30" onChange={handlechange} name="address " className="border ml-5"></textarea>
                 <br/><br/>
 
-                <button>Submit</button>
+                <button className="border rounded-lg flex-center">Submit</button>
 
             </form>
 
