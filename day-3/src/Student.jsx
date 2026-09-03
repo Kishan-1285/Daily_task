020c65bfc38e054989da9e5d818dc2d56de63dfe
kishan-1285 <kishanragunathan26@gmail.com>
2026-09-03 10:00:00 +0530

    day-3

--- day-3/src/Student.jsx
+++ day-3/src/Student.jsx
@@ -9,34 +9,61 @@ const Student = () => {
         { id: 3, name: 'Harry' }
     ]);
 
-    const [name,setName] = useState("");
+    const [name, setName] = useState("");
 
-    const handleAdd = () =>{
+    const [editId, setEditId] = useState(null);
+
+    const handleAdd = () => {
         const newList = {
-            id:list.length+1,
-            name:name,
+            id: list.length + 1,
+            name: name,
         }
-        setList([...list,newList]);
+        setList([...list, newList]);
         setName("");
     }
 
+    const handleEdit = (id) => {
+        const selectedStudent = list.find(
+            (student) => student.id === id
+        );
+        setName(selectedStudent.name);
+        setEditId(id);
+    }
+
+    const handleUpdate = () => {
+        const updatedList = list.map((student) => {
+            if (student.id === editId) {
+                return {
+                    ...student,
+                    name: name,
+                };
+            }
+            return student;
+        });
+        setList(updatedList);
+    };
+
     return (
         <>
+            <h1>List</h1>
             {list.map((students) => (
                 <div key={students.id}>
                     <h1>{students.name}</h1>
-                    <button>Edit</button>
+                    <button onClick={() => handleEdit(students.id)}>Edit</button>
                 </div>
             ))}
             <br></br>
             <input type='text'
-            placeholder='Enter your name'
-            onChange={(e)=>setName(e.target.value)}
-            value={name}
+                placeholder='Enter your name'
+                onChange={(e) => setName(e.target.value)}
+                value={name}
             />
-            <button onClick={handleAdd}>Add</button>
+            {editId === null ? (
+                <button onClick={handleAdd}>Add</button>
+            ) : (
+                <button onClick={handleUpdate}>Update</button>
+            )}
         </>
     )
 }
-
 export default Student
